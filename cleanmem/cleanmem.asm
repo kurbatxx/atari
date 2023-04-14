2386f217cd67719b1db7d5ec493986c6c3c98564
kurbatov <kurbatxx@gmail.com>
2023-04-14 14:29:24 +0600

    fix -Transfer the X register to the Stack pointer

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -2,19 +2,19 @@
 
     seg code
     org $F000           ; Define the code origin on $F000
+
 Start:
     sei                 ; Disable interputs
     cld                 ; Disable the BCD decimal math mode
     ldx #$FF            ; Loads the X register with #$FF
-    tsx                 ; Transfer the X register to the Stack pointer
+    txs                 ; Transfer the X register to the Stack pointer
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Page Zero region $00 to $FF
 ; Meaning the entire RAM and alse clear TIA registers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
     lda #0              ; A = 0
-    ldx #$FF
+    ldx #$FF            ; X = $FF
 MemLoop:
     sta $0,X            ; Store the value of A inside memory address $0 + X
     dex                 ; X--
@@ -23,7 +23,6 @@ MemLoop:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Fill the ROM size to exaсliy 4KB
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
     org $FFFC
     .word Start         ; Reset vector at $FFFC (where the program starts)
     .word Start         ; Interrupt vector at $FFFE (unused in the VCS)
