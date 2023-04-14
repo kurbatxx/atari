b9d5a5d8a358b9be4fbd6de54fab7ca25da910ca
kurbatov <kurbatxx@gmail.com>
2023-04-14 17:59:43 +0600

    solution exercise_1

--- instructions_exercises/exercise_1/exercise_1.asm
+++ instructions_exercises/exercise_1/exercise_1.asm
@@ -8,9 +8,9 @@
     org $F000       ; Define the origin of the ROM code at memory address $F000 
 Start: 
     ; TODO: 
-                    ; Load the A register with the literal hexadecimal value $82 
-                    ; Load the X register with the literal decimal value 82 
-                    ; Load the Y register with the value that is inside memory position $82 
+    lda #$82        ; Load the A register with the literal hexadecimal value $82 
+    ldx #82         ; Load the X register with the literal decimal value 82 
+    ldy $82         ; Load the Y register with the value that is inside memory position $82 
  
     org $FFFC       ; End the ROM by adding required values to memory position $FFFC 
     .word Start     ; Put 2 bytes with the reset address at memory position $FFFC 
