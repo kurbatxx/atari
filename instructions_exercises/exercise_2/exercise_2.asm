796b45f043d1c23a9e3cf53ddbde7ac7f450f7f4
kurbatov <kurbatxx@gmail.com>
2023-04-17 11:55:26 +0600

    exercise 2, 3

--- instructions_exercises/exercise_2/exercise_2.asm
+++ instructions_exercises/exercise_2/exercise_2.asm
@@ -7,10 +7,10 @@
     org $F000       ; Define the origin of the ROM code at memory address $F000 
 Start: 
     ; TODO: 
-                    ; Load the A register with the hexadecimal value $A 
-                    ; Load the X register with the binary value %11111111 
-                    ; Store the value in the A register into memory address $80 
-                    ; Store the value in the X register into memory address $81 
+    lda #$A         ; Load the A register with the hexadecimal value $A 
+    ldx #%11111111  ; Load the X register with the binary value %11111111 
+    sta $80         ; Store the value in the A register into memory address $80 
+    stx $81         ; Store the value in the X register into memory address $81 
  
     org $FFFC      ; End the ROM by adding required values to memory position $FFFC 
     .word Start    ; Put 2 bytes with the reset address at memory position $FFFC 
--- instructions_exercises/exercise_3/exercise_3.asm
+++ instructions_exercises/exercise_3/exercise_3.asm
@@ -2,20 +2,21 @@
 ; This exercise is about transferring values from registers to other registers. 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    processor 6502 
+   processor 6502 
     seg Code        ; Define a new segment named "Code" 
     org $F000       ; Define the origin of the ROM code at memory address $F000 
 Start: 
-    ; TODO: 
-                    ; Load the A register with the literal decimal value 15 
-                    ; Transfer the value from A to X 
-                    ; Transfer the value from A to Y 
-                    ; Transfer the value from X to A 
-                    ; Transfer the value from Y to A 
+    lda #15         ; Load the A register with the literal decimal value 15 
      
-                    ; Load X with the decimal value 6 
-                    ; Transfer the value from X to Y 
- 
+    tax             ; Transfer the value from A to X 
+    tay             ; Transfer the value from A to Y 
+    txa             ; Transfer the value from X to A 
+    tya             ; Transfer the value from Y to A 
+
+    ldx #6          ; Load X with the decimal value #6 
+    txa             ; Transfer X to Y 
+    tay             ;
+    
     org $FFFC      ; End the ROM by adding required values to memory position $FFFC 
     .word Start    ; Put 2 bytes with the reset address at memory position $FFFC 
     .word Start    ; Put 2 bytes with the break address at memory position $FFFE 
